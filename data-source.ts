f368325198015b56ea3d133a95e1cd99aa173816
Mathias Lovera <34527527+lovera00@users.noreply.github.com>
2022-06-16 08:02:55 -0400

    add dotenv enviroment

--- data-source.ts
+++ data-source.ts
@@ -1,13 +1,13 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-
+require('dotenv').config()
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: 'localhost',
+  type: process.env.DB_DRIVER,
+  host: process.env.DB_HOST,
   port: 3306,
-  username: 'mesita',
-  password: 'mesita',
-  database: 'mesita',
+  username: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: ['./src/models/*.{js,ts}'],
