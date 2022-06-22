ac0f1b46bd40c64c5d9045ebc67188b5d6492456
Nerea Ortiz <nmoortiz@gmail.com>
2022-06-22 01:48:36 -0400

    Cambio datasource

--- data-source.ts
+++ data-source.ts
@@ -4,8 +4,8 @@ import 'dotenv/config'
 export const AppDataSource = new DataSource({
   type: process.env.DB_DRIVER,
   host: process.env.DB_HOST,
-  port: 3306,
-  username: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
