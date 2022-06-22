d1fdd5d9624170f1e5803c843e14554e0213bf79
Nerea Ortiz <nmoortiz@gmail.com>
2022-06-22 03:38:51 -0400

    Modelo Categoria actualizado

---
+++ src/models/categoria.ts
@@ -0,0 +1,49 @@
+import 'reflect-metadata';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Mesa } from './mesa';
+import { AppDataSource } from '../../data-source';
+import { Reserva } from './reserva';
+import { Producto } from './producto';
+
+@Entity()
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  nombre!: string;
+
+  @OneToMany(() => Producto, (producto) => producto.categoria, { eager: true })
+  @JoinTable()
+  productos!: Producto[];
+
+  public static async crear(body: any): Promise<Categoria> {
+    body = (({ id, ...body }) => body)(body);
+    const categoriaRepository = AppDataSource.getRepository(Categoria);
+    return categoriaRepository.save(body);
+  }
+
+  public async actualizar(body: any): Promise<Categoria> {
+    body = (({ id, ...body }) => body)(body);
+    Object.assign(this, body);
+    const categoriaRepository = AppDataSource.getRepository(Categoria);
+    return categoriaRepository.save(this);
+  }
+
+  public async eliminar(): Promise<void> {
+    const categoriaRepository = AppDataSource.getRepository(Categoria);
+    await categoriaRepository.delete(this.id);
+  }
+
+  public static async existe(id: number): Promise<boolean> {
+    const categoriaRepository = AppDataSource.getRepository(Categoria);
+    const categoria = await categoriaRepository.findOneBy({ id });
+    return !!categoria;
+  }
+}
