c528ec38f2e51e5c6c49e7864f74f50f9fd4d4f5
Nerea Ortiz <nmoortiz@gmail.com>
2022-06-22 02:13:10 -0400

    Cambios categoria

--- src/exceptions/CategoriaNotFoundException.ts
+++ src/exceptions/CategoriaNotFoundException.ts
@@ -3,4 +3,5 @@ import HttpException from './HttpException';
 export class CategoriaNotFoundException extends HttpException {
   constructor(id: number) {
     super(404, `Categoria con id ${id} no encontrada`);
-  }}
+  }
+}
