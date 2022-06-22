45680938c7714c4691b979a6fd6ad514f9aa6f13
Nerea Ortiz <nmoortiz@gmail.com>
2022-06-22 04:31:47 -0400

    Cambios en el datasource para que corra

--- data-source.ts
+++ data-source.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import 'dotenv/config'
+import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
+
 export const AppDataSource = new DataSource({
   type: process.env.DB_DRIVER,
   host: process.env.DB_HOST,
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
   logging: false,
   entities: ['./src/models/*.{js,ts}'],
   migrations: ['./src/migrations/*.{js,ts}'],
-  subscribers: ['src/subscriber/**/*{.js,.ts}'],
-});
+  subscribers: ['src/subscriber/*/{.js,.ts}'],
+} as DataSourceOptions);
 
 AppDataSource.initialize();
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { ClienteController } from './controllers/cliente.controller';
 import { MesaController } from './controllers/mesa.controller';
 import { ReservaController } from './controllers/reserva.controller';
 import { RestauranteController } from './controllers/restaurante.controller';
-import { CategoriaController } from './controllers/categoria.controller';
 import { ProductoController } from './controllers/producto.controller';
+import { CategoriaController } from './controllers/categoria.controller';
 
 const app = new App(
   [
